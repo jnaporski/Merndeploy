87b4d96ac4f984ff03cfd1ec0a01a998412e9d60
Jay Naporski <Jaytanica777@gmail.com>
2020-06-20 19:08:52 -0500

    init upload

---
+++ client/build/precache-manifest.657b0b97c094b131fb913752bffd4467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8559aac77ac6f0729c3",
+    "url": "/static/css/main.cf6eb29c.chunk.css"
+  },
+  {
+    "revision": "c8559aac77ac6f0729c3",
+    "url": "/static/js/main.bb7e3c15.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ee313e6f96f9231eda6",
+    "url": "/static/js/2.00d70223.chunk.js"
+  },
+  {
+    "revision": "7eab51a2e3d19105faf9b079bc32a425",
+    "url": "/index.html"
+  }
+];
